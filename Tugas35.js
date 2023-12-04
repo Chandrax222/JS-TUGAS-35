a67dcb88ec3cd6887d80afe1f2c5c610be6ea0c8
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-12-04 23:56:29 +0700

    Membuat QuerySelectorAll
    Membuat QuerySelectorAll

---
+++ Tugas35.js
@@ -0,0 +1,13 @@
+function Tugas35() {
+      
+      // const lex = ['tugas','dari', 'saya','lagi']
+
+      // for (let i = 0; i < lex.length; i++) {
+      //       const element = lex[i];
+         
+      //       console.log(element);
+      // }
+      console.log(document.querySelectorAll(".tugas"));
+
+}
+Tugas35();
